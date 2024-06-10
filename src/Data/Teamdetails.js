592121f6531f4575159c53045fd9d955a446e8ff
Gosith Jayathilake <j.gosith@gtngroup.com>
2024-06-10 08:22:42 +0530

    before adding declr forms

--- src/Data/Teamdetails.js
+++ src/Data/Teamdetails.js
@@ -1,10 +1,12 @@
 import Shehanimg from "../assets/shehan.png";
+import Isuruimg from "../assets/isuru.JPG";
 import Dulkyimg from "../assets/Dulky.jpg";
 import Kethakiimg from "../assets/Kethaki.jpg";
 import Maryamimg from "../assets/Maryam.jpg";
 import Shaneliimg from "../assets/Shaneli.png";
 import Dineliimg from "../assets/Dineli.png";
-
+import Warunaimg from "../assets/Waruna.jpg";
+import hasithaimg from "../assets/hasitha.jpg";
 
 export const Teamdetails = [
     {
@@ -23,16 +25,16 @@ export const Teamdetails = [
                 description: "“A mortgage is a relationship, not a transaction”. I have worked in the banking and finance industry for over 20 years with both local and international experience. In my previous role, I gained expertise in first home loans, and I want to make your stride toward owning a home/ acquiring an investment property a happy, pleasant, and positive experience. Getting the correct advice is the most important factor in lending. I can direct you with the best advice and give tailor-made solutions for your mortgage needs. I believe, that treating a customer right is more important than any business gain. I look forward to directing you to achieve the dream of owning your First Home in New Zealand, purchasing your Investment Property, Refixing your current mortgage, or Refinancing your mortgage. We have access to a wide panel of major banks and I’m sure we can get you the most suitable option with these banks. Sometimes you might not be fortunate enough to have a 20% deposit to purchase your first home. With my experience with numerous Kainga Ora applications completed, I believe I can get your loan approved if you meet the Kainga Ora eligibility.",
 
             }, {
-                pp: {},
+                pp: Isuruimg,
                 role: "KiwiSaver Support",
                 name: "Isuru Rodrigo",
-                description: "I am a flexible person with good communication skills, hardworking, enthusiastic, team player and a fast learner with positive attitude of yes, I/we can. Love to learn new things, as it helps me to improve myself and become a better version of me. I have completed my bachelor's degree from Sabaragamuwa University of Sri Lanka majoring in Banking and Insurance and have professional academic background in Accounting, finance and banking areas. I enjoy watching movies and TV shows, reading books, travelling and learning different languages. It helps me to learn about different cultures and people. At Aarkashaa, I'm  working with both Life & health insurance and Mortgage teams. Also, looks after the compliance requirements of the company set by FMA. I would like to knock on new opportunities with an environment helps to enhance and improve my skills and learning experience to go forward in my career path.",
+                description: "With over 12 years in the banking and finance industry and 21 years in global equity markets, I bring extensive expertise and passion to my role at Aarakshaa. As Head of Broker Support, I oversee a team dedicated to providing brokers with the tools, resources, and guidance they need to succeed in complex financial markets. My career is marked by a strong track record in market analysis, risk management, and strategic planning. This experience ensures that I stay ahead of market trends and developments, continuously seeking innovative solutions for our clients. I am committed to delivering exceptional service, fostering strong industry relationships, and maintaining the highest standards of integrity and transparency. At Aarakshaa, our goal is to exceed client expectations and support brokers in navigating the ever-evolving financial landscape.",
 
             }, {
-                pp: {},
+                pp: Warunaimg,
                 role: "Head of Education",
-                name: "Waruna",
-                description: "A highly motivated, creative all rounder with adequate competencies and a good eye detail. I am confident, positive and flexible with a “can-do” attitude. I believe that my planning and organizing skills have enabled me to give effective results in meeting deadlines. I am also a fast learner with strong communication skills. An Accounting personnel with finance background who is pursuing ACCA. I am not only an accounting person but also have experience in the field of risk management and insurance proficiency. My educational journey provided me with a solid foundation in these various areas of risk management, accounting and insurance. I have 2 years of experience in one of the big audit firms in Sri Lanka & Maldives, and many years of experience in the finance and insurance sector based in Sri Lanka and Australia respectively.. ",
+                name: "Waruna Tennekoon",
+                description: "Waruna is a Banking professional from Sri Lanka. He worked at HSBC Middle East in UAE for nearly a decade. Waruna came to New Zealand in 2019 and joined the logistics industry, quickly making his way to becoming a Planning & Distribution Manager in a leading multinational company. Waruna got the opportunity to meet a lot of people who came for studies, hoping to make New Zealand their home. This prompted him to help these students in realizing their dreams in New Zealand. This is one main reason for him to get into education consultancy. Living in New Zealand for over 5 years gives him the ground reality of what to expect and what needs to be done to fulfill student goals & dreams. He is dedicated & committed to choosing the right education and career pathways for International students.",
 
             }
         ]
@@ -55,10 +57,10 @@ export const Teamdetails = [
                 description: "Our youngest member ( just by 40 seconds), Dineli Saparamadu, is a newly qualified Mortgage Advisor working hard to gain industry experience while completing a BCom degree. She is a dedicated and hard working individual with a fun and energetic personality. She took a gap year with her sister in 2023 where she participated in a Europe Tour covering over 22 countries over 2 months. While she had an amazing time and gained valuable life experiences and skills through her travels, she also completed her Level 5 Residential Lending Qualification. She is greatly looking to come forward in the industry and does not doubt doing so as she knows she has the enduring support of our Aarakshaa team.",
 
             }, {
-                pp: {},
-                name: "Hasitha",
-                role: "Insurance Support",
-                description: "Our youngest member ( just by 40 seconds), Dineli Saparamadu, is a newly qualified Mortgage Advisor working hard to gain industry experience while completing a BCom degree. She is a dedicated and hard working individual with a fun and energetic personality. She took a gap year with her sister in 2023 where she participated in a Europe Tour covering over 22 countries over 2 months. While she had an amazing time and gained valuable life experiences and skills through her travels, she also completed her Level 5 Residential Lending Qualification. She is greatly looking to come forward in the industry and does not doubt doing so as she knows she has the enduring support of our Aarakshaa team.",
+                pp: hasithaimg,
+                name: "Hasitha Samarawickrama",
+                role: "Broker Support - Insurance",
+                description: "Hasitha Sanjeewa Samarawickrama is a dynamic and accomplished professional with 19 years of extensive experience in the Bancassurance/Insurance sector, specializing in both Life and General Insurance in Sri Lanka. Hasitha started his Bancassurance career with HNB Assurance. He worked as the Head of Bancassurance at LOLC Life Assurance, the fastest-growing insurance company in Sri Lanka and LOLC Holdings PLC part of the largest conglomerate in the country. With a strong passion for excellence and a deep understanding of the insurance industry, Hasitha has proven expertise in product development, channel establishment, business partnerships, team leadership, and performance management. Hasitha's academic background includes an MBA with a specialization in Marketing from the University of Bedfordshire, UK, as well as certificates in marketing, professional selling, and business computing from reputable institutions such as the Sri Lanka Institute of Marketing (SLIM), London Centre of Marketing, UK, and the Association of Accounting Technicians.",
 
             }
         ]
